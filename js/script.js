980b16356e9458b44af4baea7b577f411392bbfe
Brandon Ladson <brandonladson1999@gmail.com>
2020-10-26 17:38:55 +0100

    rifinitura regole css e js

--- js/script.js
+++ js/script.js
@@ -4,14 +4,17 @@
 var numeriLista = "";
 for (var i = 1; i <= 100; i++) {
 
-  if (i % 15 === 0) {
+  // definire multipli di 3 e 5
+  if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz");
     numeriLista = numeriLista + "<li>" + "FizzBuzz" + "</li>";
 
+    // definire multipli di 3
   } else if (i % 3 === 0) {
     console.log("Fizz");
     numeriLista = numeriLista + "<li>" + "Fizz" + "</li>";
 
+    // definire multipli di 5
   } else if (i % 5 === 0) {
     console.log("Buzz");
     numeriLista = numeriLista + "<li>" + "Buzz" + "</li>";
@@ -21,5 +24,6 @@ for (var i = 1; i <= 100; i++) {
     numeriLista = numeriLista + "<li>" + i + "</li>";
   }
 
+  // stampare lista di numeri
   document.getElementById('lista').innerHTML = numeriLista;
 }
